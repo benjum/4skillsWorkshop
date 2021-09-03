c0881983dd175d85aa4b404640283a42e782f8f4
Benjamin John Winjum <bwinjum@ucla.edu>
2021-09-03 16:53:25 -0700

    first commit

---
+++ book/_build/jupyter_execute/chap03.py
@@ -0,0 +1,11 @@
+# Chapter 3
+
+This is a Jupyter Notebook.  This notebook contains sample Python codes.
+
+print("Hello world!")
+
+L = [1, 2, 3]
+sum = 0
+for num in L:
+    sum = sum + num
+print('Sum =', sum)
